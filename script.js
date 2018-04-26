fd504a44e3883ce6bdf6c2540934dc64084c7e1e
ChessKnock <rusmeditor@gmail.com>
2018-04-26 20:10:02 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let startBtn = document.getElementById("start-btn");
+
+startBtn.onclick = function() {
+  startBtn.setAttribute("hide", null);
+  
+  for(let temp of document.getElementsByClassName("article-field")) {
+    temp.removeAttribute("hide");
+  }
+
+  creatCells();
+};
+
+function creatCells() {
+  let tempRow;
+  for(let i = 0; i < 9; i++) {
+    tempRow = document.createElement("div");// присваеваем новый елемент
+
+    for(let j = 0; j < 9; j++) {
+      tempRow.appendChild(document.createElement("div")).className = `cells r-${i + 1} c-${j + 1}`;
+    }
+    // добавляем на HTML документ
+    document.querySelector("#left-field").appendChild(tempRow).className = `rows row-${i + 1}`;
+  }
+}
+
+
+
+
